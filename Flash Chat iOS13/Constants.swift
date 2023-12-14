b794d12db99f3c76c2d947b5006647c0190e347c
AdityaBoghara <bogharaaditya@gmail.com>
2023-12-13 17:47:40 -0700

    Added Logout button and all necessary changes. Added constant file.

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Aditya Boghara on 12/13/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
--- Podfile
+++
@@ -1,9 +0,0 @@
-platform :ios, '15.0'
-
-target 'Flash Chat iOS13' do
-  use_frameworks!
-
-#pod 'FirebaseAuth'
-#pod 'FirebaseFirestore'
-
-end
