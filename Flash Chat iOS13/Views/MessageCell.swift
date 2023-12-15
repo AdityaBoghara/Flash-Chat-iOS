a7ed11ba7cd67a1400905b97262b73e77822857f
AdityaBoghara <bogharaaditya@gmail.com>
2023-12-14 22:38:46 -0700

    retrive data from firestore

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Aditya Boghara on 12/14/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var rigthImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '15.0'
+
+target 'Flash Chat iOS13' do
+  use_frameworks!
+
+#pod 'FirebaseAuth'
+#pod 'FirebaseFirestore'
+
+end
